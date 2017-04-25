f7e830d23b9e74c2d9a82961e9e930d0c624c7b1
Akshay <akshay@punchh.com>
2017-04-25 18:45:10 +0530

    optimise the code for setting data in listview

--- src/Home.js
+++ src/Home.js
@@ -18,18 +18,14 @@ export default class Home extends Component {
   
   constructor(props) {
     super(props);
-    
-    moivesArr=this.props.response.data.movies;
-    stringArr=moivesArr;
-    
-    for(k=0;k<moivesArr.length;k++){
-      stringArr[k]=moivesArr[k].title;
-    }
-    
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      dataSource: ds.cloneWithRows(stringArr)
+      dataSource: ds.cloneWithRows(this.props.response.data.movies)
     };
+  }
+  
+  renderRow(rowData) {
+    return <Text style={{ color: 'red', fontWeight: 'bold', marginHorizontal: 10, fontSize: 20 }}>{rowData.title}</Text>
   }
   
   render() {
@@ -37,7 +33,7 @@ export default class Home extends Component {
     <View style={{flex: 1, paddingTop: 22}}>
     <ListView
       dataSource={this.state.dataSource}
-      renderRow={(rowData) => <Text>{rowData}</Text>}
+      renderRow={this.renderRow.bind(this)}
     />
       </View>
     );
